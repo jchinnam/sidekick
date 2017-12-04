327d6970bc057d0ff102fbf160b034ae954155c5
jchinnam <jchinnam@umich.edu>
2017-12-04 10:41:55 -0500

    add google.py

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 includes
 - `commands.txt` for bash commands
 - `bin/` for bash scripts
-	- `trash`
-	- `up`
+	- `trash` empties trash
+	- `up` cd up `n` directories
 - `py/` for python scripts
+	- `google` for a quick g search from terminal
+
+feel free to adopt/share!
---
+++ py/google.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# google something
+
+import webbrowser
+import sys
+
+search_phrase = ""
+for i in range(1, len(sys.argv)):
+	search_phrase += " "
+	search_phrase += sys.argv[i]
+
+url = "https://www.google.com.tr/search?q={}".format(search_phrase) 
+b = webbrowser.get('safari')
+b.open(url)
