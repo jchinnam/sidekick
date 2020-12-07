5f5ca118788a7dcbd9dec5095a6ae587de764cfc
jchinnam <jchinnam@umich.edu>
2020-12-07 00:56:11 -0500

    clean up requests

--- py/pokemon.py
+++ py/pokemon.py
@@ -4,23 +4,18 @@
 import requests
 import random
 
+def request(url, payload):
+    response = requests.request("GET", url, data=payload)
+    return response.json()
+
 # get total num of pokemon to date
-url = "https://pokeapi.co/api/v2/pokedex/national"
-
-payload = ""
-response = requests.request("GET", url, data=payload)
-all_pokemon = response.json()
-
+all_pokemon = request("https://pokeapi.co/api/v2/pokedex/national", "")
 num_pokemon = len(all_pokemon["pokemon_entries"])
 
 rand_pokemon_id = random.randint(1,num_pokemon - 1)  # from 1 to num_pokemon, inclusive
 
-url = "http://pokeapi.co/api/v2/pokemon/" + str(rand_pokemon_id) + "/"
-
-payload = ""
-response = requests.request("GET", url, data=payload)
-data = response.json()
-
-name = data["forms"][0]["name"]
+# get specific pokemon
+pokemon = request("http://pokeapi.co/api/v2/pokemon/" + str(rand_pokemon_id) + "/", "")
+name = pokemon["forms"][0]["name"]
 
 print(name + " says hello")
